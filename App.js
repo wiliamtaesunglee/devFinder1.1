2d11a0878d9c6b54699e78f91bf66a6de05a7cbf
wil.i.am <wiliam.tae@gmail.com>
2020-03-12 08:27:46 -0300

    deep linking added

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, useLinking } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { View, Text } from 'react-native'
 
 import LoginScreen from './src/screens/loginScreen.js';
 import DevListScreen from './src/screens/devListScreen.js';
@@ -10,8 +9,45 @@ import DevListScreen from './src/screens/devListScreen.js';
 const Stack = createStackNavigator()
 
 export default function App() {
+    const ref = React.useRef();
+
+    const { getInitialState } = useLinking(ref, {
+        prefixes: ['http://localhost:8081', 'devfinder://'],
+        config: {
+            DevList: 'devlist',
+        },
+    });
+
+    const [isReady, setIsReady] = React.useState(false);
+    const [initialState, setInitialState] = React.useState();
+
+    React.useEffect(() => {
+        Promise.race([
+            getInitialState(),
+            new Promise(resolve =>
+                // Timeout in 150ms if `getInitialState` doesn't resolve
+                // Workaround for https://github.com/facebook/react-native/issues/25675
+                setTimeout(resolve, 150)
+            ),
+        ])
+            .catch(e => {
+                console.error(e);
+            })
+            .then(state => {
+                if (state !== undefined) {
+                    setInitialState(state);
+                }
+
+                setIsReady(true);
+            });
+    }, [getInitialState]);
+
+    if (!isReady) {
+        return null;
+    }
+
   return (
-    <NavigationContainer>
+    <NavigationContainer initialState={initialState} ref={ref}>
       <Stack.Navigator>
         <Stack.Screen
           name='DevFinder'
--- src/screens/loginScreen.js
+++ src/screens/loginScreen.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, Linking } from 'react-native';
+
+const fetchGit = async () => {
+    await fetch('http://github.com/login/oauth/authorize', { client_id: '4fe64420755fe63ff364' })
+        .then(resp => {
+            Linking.openURL(resp.url).catch((err) => console.error('An error occurred', err));
+        })
+        .then(resp => console.log(resp.json()))
+}
 
 const LoginScreen = ({ navigation }) => {
   return (
@@ -7,7 +15,7 @@ const LoginScreen = ({ navigation }) => {
       <Text>Clicke no Botão para logar no seu Github</Text>
       <Button
         title='Login'
-        onPress={() => navigation.navigate('DevList')} />
+        onPress={() => fetchGit()} />
     </View>
   );
 };
